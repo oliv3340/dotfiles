17f547cb9a3d0e15cbdf3764750656c02bd451d6
Olivier GRACIANNETTE <olivier.graciannette@mirakl.com>
2022-02-16 15:46:00 +0100

    fix pygmentize

--- fish/config-osx.fish
+++ fish/config-osx.fish
@@ -6,13 +6,15 @@ end
 
 alias mkdir "mkdir -p"
 if command -qv pygmentize
-  alias cat "pygmentize -O style=one-dark"
+  alias cat "pygmentize -gO style=one-dark"
 end
 alias kg "kubectl get"
 alias kd "kubectl delete"
 alias kds "kubectl describe"
 alias kl "kubectl logs"
 alias ka "kubectl apply"
+
+alias fishr "source $HOME/.config/fish/config.fish"
 
 export GOPATH="$HOME/Documents/dev/languages/go"
 export GOMODCACHE="$HOME/Documents/dev/languages/go/pkg/mod"
